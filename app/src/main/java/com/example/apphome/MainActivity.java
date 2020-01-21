7f6339fad0e7674f9a94027f34128ebc0bb80020
franciscozavalarosado <fzavala@utcam.edu.mx>
2020-01-20 23:53:14 -0600

    nuevo diseño1

--- app/src/main/java/com/example/apphome/MainActivity.java
+++ app/src/main/java/com/example/apphome/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.apphome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtView1=findViewById(R.id.txtMain);
+        txtView1.setText("Manipulando Texto");
+
     }
 }
